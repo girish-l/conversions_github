63f0f1963ea56eeed071591b1315cc60c0bc9305
Girish L <girish.l@hotmail.com>
2022-10-16 19:57:04 +1100

    Update from pest to phpuit

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,5 +1,14 @@
 <?php
 
-it('can test', function () {
-    expect(true)->toBeTrue();
-});
+namespace Girishl\Conversions\Tests;
+
+class ExampleTest extends TestCase
+{
+    /**
+    * @test
+    */
+    public function it_returns_true(): void
+    {
+        $this->assertFalse(false);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Girishl\Conversions\Tests;
+
+class TestCase extends \PHPUnit\Framework\TestCase
+{
+
+}
